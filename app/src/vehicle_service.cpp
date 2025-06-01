09e91e476463a3c7cb9d04de3d6ab08c0d00e60f
Patryk Czuchnowski <patczuch@gmail.com>
2025-06-01 14:23:55 +0200

    Init logs and metrics

--- app/src/vehicle_service.cpp
+++ app/src/vehicle_service.cpp
@@ -18,7 +18,37 @@
 #include <opentelemetry/exporters/otlp/otlp_grpc_exporter.h>
 #include <opentelemetry/sdk/trace/simple_processor.h>
 #include <opentelemetry/sdk/trace/tracer_provider.h>
-#include <opentelemetry/trace/provider.h>
+
+#include "opentelemetry/exporters/otlp/otlp_http_exporter_factory.h"
+#include "opentelemetry/exporters/otlp/otlp_http_exporter_options.h"
+#include "opentelemetry/sdk/trace/processor.h"
+#include "opentelemetry/sdk/trace/batch_span_processor_factory.h"
+#include "opentelemetry/sdk/trace/batch_span_processor_options.h"
+#include "opentelemetry/sdk/trace/tracer_provider_factory.h"
+#include "opentelemetry/trace/provider.h"
+#include "opentelemetry/sdk/trace/tracer_provider.h"
+
+#include "opentelemetry/exporters/otlp/otlp_http_metric_exporter_factory.h"
+#include "opentelemetry/exporters/otlp/otlp_http_metric_exporter_options.h"
+#include "opentelemetry/metrics/provider.h"
+#include "opentelemetry/sdk/metrics/aggregation/default_aggregation.h"
+#include "opentelemetry/sdk/metrics/export/periodic_exporting_metric_reader.h"
+#include "opentelemetry/sdk/metrics/export/periodic_exporting_metric_reader_factory.h"
+#include "opentelemetry/sdk/metrics/meter_context_factory.h"
+#include "opentelemetry/sdk/metrics/meter_provider.h"
+#include "opentelemetry/sdk/metrics/meter_provider_factory.h"
+
+#include "opentelemetry/exporters/otlp/otlp_http_log_record_exporter_factory.h"
+#include "opentelemetry/exporters/otlp/otlp_http_log_record_exporter_options.h"
+#include "opentelemetry/logs/provider.h"
+#include "opentelemetry/sdk/logs/logger_provider_factory.h"
+#include "opentelemetry/sdk/logs/processor.h"
+#include "opentelemetry/sdk/logs/simple_log_record_processor_factory.h"
+
+#include "opentelemetry/sdk/metrics/view/instrument_selector_factory.h"
+#include "opentelemetry/sdk/metrics/view/meter_selector_factory.h"
+#include "opentelemetry/sdk/metrics/view/view_factory.h"
+
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -40,6 +70,14 @@ namespace trace_sdk = opentelemetry::sdk::trace;
 namespace trace_api = opentelemetry::trace;
 namespace otlp_exporter = opentelemetry::exporter::otlp;
 
+namespace metrics_sdk      = opentelemetry::sdk::metrics;
+namespace metrics_api      = opentelemetry::metrics;
+
+namespace otlp = opentelemetry::exporter::otlp;
+
+namespace logs_api = opentelemetry::logs;
+namespace logs_sdk = opentelemetry::sdk::logs;
+
 struct VehicleLocation {
     double latitude;
     double longitude;
@@ -51,7 +89,7 @@ private:
     std::unordered_map<int32_t, std::vector<Location>> vehicle_locations_;
     std::unordered_map<int32_t, int32_t> delivered_packages_;
 	std::unique_ptr<PackageService::Stub> package_stub_;
-	
+
     struct TrackData {
     std::mutex track_mutex;
     std::condition_variable cv;
@@ -63,11 +101,11 @@ private:
 public:
 	VehicleServiceImpl(std::shared_ptr<grpc::Channel> package_channel)
         : package_stub_(packages::PackageService::NewStub(std::static_pointer_cast<grpc::ChannelInterface>(package_channel))) {}
-		
+
     Status sendLocation(ServerContext* context,
                     ServerReader<Location>* reader,
                     Ack* response) override {
-	
+
         Location loc;
         int32_t vehicle_id = 0;
         int location_count = 0;
@@ -208,12 +246,64 @@ void initTracer()
     trace_api::Provider::SetTracerProvider(provider);
 }
 
+void AddLatencyView(opentelemetry::sdk::metrics::MeterProvider* provider,
+                    const std::string& name, const std::string& unit) {
+    auto histogram_config = std::make_shared<
+    opentelemetry::sdk::metrics::HistogramAggregationConfig>();
+    histogram_config->boundaries_ = {
+        0,     0.00001, 0.00005, 0.0001, 0.0003, 0.0006, 0.0008, 0.001, 0.002,
+        0.003, 0.004,   0.005,   0.006,  0.008,  0.01,   0.013,  0.016, 0.02,
+        0.025, 0.03,    0.04,    0.05,   0.065,  0.08,   0.1,    0.13,  0.16,
+        0.2,   0.25,    0.3,     0.4,    0.5,    0.65,   0.8,    1,     2,
+        5,     10,      20,      50,     100};
+        provider->AddView(
+            opentelemetry::sdk::metrics::InstrumentSelectorFactory::Create(
+                opentelemetry::sdk::metrics::InstrumentType::kHistogram, name, unit),
+                opentelemetry::sdk::metrics::MeterSelectorFactory::Create(
+                    "grpc-c++", grpc::Version(), ""),
+                          opentelemetry::sdk::metrics::ViewFactory::Create(
+                              name, "", unit,
+                              opentelemetry::sdk::metrics::AggregationType::kHistogram,
+                              std::move(histogram_config)));
+}
+
+void initMetrics()
+{
+    otlp::OtlpHttpMetricExporterOptions opts;
+    opts.url = "simplest-collector:4318/v1/metrics";
+    auto exporter = otlp::OtlpHttpMetricExporterFactory::Create(opts);
+    metrics_sdk::PeriodicExportingMetricReaderOptions reader_options;
+    reader_options.export_interval_millis = std::chrono::milliseconds(1000);
+    reader_options.export_timeout_millis  = std::chrono::milliseconds(500);
+    auto reader = metrics_sdk::PeriodicExportingMetricReaderFactory::Create(std::move(exporter), reader_options);
+    auto context = metrics_sdk::MeterContextFactory::Create();
+    context->AddMetricReader(std::move(reader));
+    auto u_provider = metrics_sdk::MeterProviderFactory::Create(std::move(context));
+    AddLatencyView(u_provider.get(), "grpc.server.call.duration", "s");
+    std::shared_ptr<metrics_api::MeterProvider> provider(std::move(u_provider));
+    metrics_api::Provider::SetMeterProvider(provider);
+
+}
+
+void initLogger()
+{
+    otlp::OtlpHttpLogRecordExporterOptions opts;
+    opts.url = "simplest-collector:4318/v1/logs";
+    auto exporter  = otlp::OtlpHttpLogRecordExporterFactory::Create(opts);
+    auto processor = logs_sdk::SimpleLogRecordProcessorFactory::Create(std::move(exporter));
+    std::shared_ptr<logs_api::LoggerProvider> provider =
+    logs_sdk::LoggerProviderFactory::Create(std::move(processor));
+    logs_api::Provider::SetLoggerProvider(provider);
+}
+
 int main(int argc, char** argv) {
     std::string server_address("0.0.0.0:50052");
     std::string package_service_address("package-service:50052");
-	initTracer();
-	auto package_channel = grpc::CreateChannel(package_service_address, grpc::InsecureChannelCredentials());
-	
+    auto package_channel = grpc::CreateChannel(package_service_address, grpc::InsecureChannelCredentials());
+    initTracer();
+    initMetrics();
+    initLogger();
+
     VehicleServiceImpl service(package_channel);
 
     ServerBuilder builder;
@@ -224,6 +314,6 @@ int main(int argc, char** argv) {
     std::cout << "VehicleService server listening on " << server_address << std::endl;
 
     server->Wait();
-	
+
     return 0;
 }
